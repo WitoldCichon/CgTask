632cc312e67f92894a869a957d4ab7f4542cdf70
Witold Cichoń <witold.cichonn@gmail.com>
2018-11-12 14:04:03 +0100

    Create vertex shader object and draw a quad

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "cg_gl_error_checker.h"
+#include "shader.h"
 
 #include <GLFW/glfw3.h>
 
@@ -24,10 +24,23 @@ int main() {
   CG_DEBUG(mainCgContext = cgCreateContext());
   CG_DEBUG(cgGLSetDebugMode(CG_TRUE)); // user OpenGL error checking
 
+  // Create vertex shader
+  Shader vertexShader(mainCgContext,
+                      {"../resources/shaders/vertex_shader.cg", "main_function", CG_GL_VERTEX});
+
   /* Loop until the user closes the window */
   while (!glfwWindowShouldClose(window)) {
     /* Render here */
     glClear(GL_COLOR_BUFFER_BIT);
+
+    vertexShader.bind();
+
+    glBegin(GL_QUADS);
+    glVertex2f(-0.8f, -0.8f);
+    glVertex2f(0.8f, -0.8f);
+    glVertex2f(0.8f, 0.8f);
+    glVertex2f(-0.8f, 0.8f);
+    glEnd();
 
     /* Swap front and back buffers */
     glfwSwapBuffers(window);
